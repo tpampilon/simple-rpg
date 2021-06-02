4bcba0f1b032af33db684a65e3f93b9d840ab14d
Ted <ted@ted-ubuntu-desktop>
2021-06-02 15:08:33 -0600

    add use-key-press and use-walk hooks

--- src/components/player/index.jsx
+++ src/components/player/index.jsx
@@ -1,15 +1,25 @@
 import Actor from "../actor";
+import useKeyPress from "../../hooks/use-key-press";
+import useWalk from "../../hooks/use-walk";
 
 const Player = ({skin}) => {
-  
+
+  const { dir, step, walk } = useWalk(3);
+
   const data = {
     h: 32,
     w: 32
   }
 
+  useKeyPress((event) => {
+    event.preventDefault();
+    const dir = event.key.replace("Arrow", "").toLowerCase();
+    walk(dir)
+  })
+
   return (
     <div className="zone-container">
-      <Actor sprite={`/sprites/skins/${skin}.png`} data={data} />
+      <Actor sprite={`/sprites/skins/${skin}.png`} data={data}  step={step} dir={dir}/>
     </div>
   );
 
--- src/hooks/use-key-press/index.js
+++ src/hooks/use-key-press/index.js
@@ -2,6 +2,9 @@ import { useEffect } from "react";
 
 const useKeyPress = (fn) => {
   useEffect(() => {
-    
+    window.addEventListener("keydown", fn);
+    return () => window.EventListener("keydown", fn);
   }, [])
 }
+
+export default useKeyPress;
