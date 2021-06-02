a83281157bf4f16d1406c1f0efea9eb29cf48ea3
Ted <ted@ted-ubuntu-desktop>
2021-06-01 23:28:48 -0600

    add sprite component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Sprite from "./components/sprite";
+
+const App = () => {
+  return (
+    <div className="zone-container">
+      <Sprite />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sprite/index.jsx
@@ -0,0 +1,20 @@
+
+const Sprite = () => {
+
+  return ( 
+    <div
+      style={{
+        display: "inline-block",
+        height: "32px",
+        width: "32px",
+        backgroundImage: "url(/sprites/m1.png)",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "0px 0px",
+        
+      }}
+   />
+  )
+
+}
+
+export default Sprite;
